6107efeb0064134dfb6dc7c648e3a1f61c092db2
Aminul Islam <aminulabir2004@gmail.com>
2024-10-02 23:16:53 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,27 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          
+        },
+        maxWidth:{
+            'container':'1320px'
+        },
+        backgroundImage: {
+            'menu': "url('/images/header.png')",
+            'first': "url('/images/first.png')",
+            'second': "url('/images/second.png')",
+            'third': "url('/images/third.png')",
+            'footer': "url('/images/footer.png')",
+        },
+        fontFamily: {
+            'plus': ['Plus Jakarta Sans'],
+            'nothing':['Nothing You Could Do']
+            
+        },
+        margin: {
+            '-50px': '-50px',
+          }
+      }
+    }
+  }
